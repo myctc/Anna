78d2de0b5931de71582e3288a500f461c7ee9749
myctc <154863245+myctc@users.noreply.github.com>
2026-07-12 00:40:27 +0530

    Add homepage entry point

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = 'Anna Prithibi Seed Farm';
+include 'includes/header.php';
+?>
+<div class="container py-5">
+  <div class="row align-items-center">
+    <div class="col-lg-6">
+      <h1 class="display-4 text-success fw-bold">Anna Prithibi Seed Farm</h1>
+      <p class="lead">Quality Seeds for Better Harvests</p>
+      <a href="pages/products.php" class="btn btn-success btn-lg">Explore Products</a>
+    </div>
+    <div class="col-lg-6">
+      <img src="assets/images/hero.jpg" class="img-fluid" alt="Seed Farm">
+    </div>
+  </div>
+</div>
+<?php include 'includes/footer.php'; ?>
