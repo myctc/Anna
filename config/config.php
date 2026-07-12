d24159bd2a67ffd6b9e061b79deaeb115a8e6283
myctc <154863245+myctc@users.noreply.github.com>
2026-07-12 13:25:14 +0530

    Add application configuration

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+date_default_timezone_set('Asia/Dhaka');
+define('SITE_NAME','Anna Prithibi Seed Farm');
+define('BASE_URL','http://localhost/Anna/');
+require_once __DIR__.'/database.php';
