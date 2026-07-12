e051e9c92b576888ee79ca3854d9fa17be3b1eef
myctc <154863245+myctc@users.noreply.github.com>
2026-07-12 13:31:49 +0530

    Add product delete handler

---
+++ admin/products/delete.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../../config/config.php';
+if(!isset($_SESSION['admin'])){header('Location: ../login.php');exit;}
+if(isset($_GET['id'])){
+ $id=(int)$_GET['id'];
+ $conn->query("DELETE FROM products WHERE id=$id");
+}
+header('Location: index.php');
+exit;
